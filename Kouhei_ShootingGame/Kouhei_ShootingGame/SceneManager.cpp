e1ffea1c3a70cd64e7d5324c4da2044287e1d2db
maruyamakouhei <107020852+marukoisan@users.noreply.github.com>
2022-10-11 11:56:19 +0900

    シーンマネージャー処理の追加

---
+++ Kouhei_ShootingGame/Kouhei_ShootingGame/SceneManager.cpp
@@ -0,0 +1,30 @@
+#include"SceneManager.h"
+
+//メンバ変数の値を書き換えるのがアップデート関数
+void SceneManager::Update()
+{
+	//現在のシーンのアップデートを実行
+    mScene->Update();
+}
+
+//描画処理のみを行う関数
+void SceneManager::Draw() const
+{
+	mScene->Draw();//現在シーンの描画関数を実行
+}
+
+//シーンの変更処理
+AbstractScene* SceneManager::ChangeScene()
+{
+	//シーンの変更処理呼び出し
+	AbstractScene* nextScene = mScene->ChangeScene();
+
+	//次のシーンが現在のシーンと別なら変更
+	if (nextScene != mScene)
+	{
+		delete mScene;
+		mScene = nextScene;
+	}
+
+	return nextScene;
+}
